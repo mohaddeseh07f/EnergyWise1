648f194ca439d317a3c4dce4a80ac193c39d4db1
mohaddeseh07f <m.farahani0741826@gmail.com>
2025-12-26 08:46:29 +0330

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "your-project.firebaseapp.com",
-  databaseURL: "https://your-project-default-rtdb.firebaseio.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef1234567890"
+  apiKey: "AIzaSyBfWtOyGeXPl1m6lBxw7g-94AynEowuEBk",
+  authDomain: "energywise-comments.firebaseapp.com",
+  projectId: "energywise-comments",
+  storageBucket: "energywise-comments.firebasestorage.app",
+  messagingSenderId: "879747563602",
+  appId: "1:879747563602:web:380a4ed86f62cbb6ec5f8b",
+  measurementId: "G-BNGSX4FXF5"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
